fb2943c0fbccc3d1756688b2af469bba58fe3311
Harsh Agarwal <harshag.code@gmail.com>
2025-03-26 23:43:47 +0530

    .

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
+import multer from 'multer'
 dotenv.config();
 
 const app = express();
